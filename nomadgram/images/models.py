0b1f86c1423e0512170fe05700b8d246b17ee51a
seon54 <seonhyeongpark54@gmail.com>
2019-05-03 15:13:32 +0900

    modify models.py

--- nomadgram/images/models.py
+++ nomadgram/images/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from nomadgram.users import models as user_models
 
-# Create your models here.
+
+class  TimeStampedModel(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True) 
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Image(TimeStampedModel):
+
+    file = models.ImageField()
+    location = models.CharField(max_length=140)
+    caption = models.TextField()
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
+
+
+class Comment(TimeStampedModel):
+
+    message = models.TextField()
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
+    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
+
+
+class Like(TimeStampedModel):
+
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
+    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
