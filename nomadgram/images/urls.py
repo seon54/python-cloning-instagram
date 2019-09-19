e5658d56a530615c01846759b02645e29ebea2f1
shpark <jlk@shpark.local>
2019-09-19 11:07:22 +0900

    commit

---
+++ nomadgram/images/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from .views import *
+
+
+urlpatterns = [
+    path('', Feed.as_view(), name='feed'),
+    path('<int:image_id>/like', LikeImage.as_view(), name='like_image'),
+    path('<int:image_id>/comment', CommentOnImage.as_view(), name='comment_image'),
+    path('comments/<int:comment_id>', Comment.as_view(), name='comment'),
+
+]
---
+++ nomadgram/images/views.py
@@ -0,0 +1,76 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import *
+from .serializers import *
+
+
+class Feed(APIView):
+
+    def get(self, request, format=None):
+        user = request.user
+        following_users = user.following.all()
+
+        image_list = []
+
+        for following_user in following_users:
+            user_image = following_user.images.all()[:2]
+
+            for image in user_image:
+                image_list.append(image)
+
+        sorted_list = sorted(image_list, key=lambda image: image.created_at, reverse=True)
+        serializer = ImageSerializer(sorted_list, many=True)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+class LikeImage(APIView):
+
+    def get(self, request, image_id, format=None):
+        user = request.user
+
+        try:
+            found_image = Image.objects.get(id=image_id)
+        except Image.DoesNotExist:
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        try:
+            preexisting_like = Like.objects.get(creator=user, image=found_image)
+            preexisting_like.delete()
+        except Like.DoesNotExist:
+            new_like = Like.objects.create(creator=user, image=found_image)
+            new_like.save()
+
+        return Response(status=status.HTTP_201_CREATED)
+
+
+class CommentOnImage(APIView):
+
+    def post(self, request, image_id, format=None):
+        user = request.user
+
+        try:
+            found_image = Image.objects.get(id=image_id)
+        except Image.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(creator=user, image=found_image)
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class Comment(APIView):
+
+    def delete(self, request, comment_id, format=None):
+        user = request.user
+        try:
+            comment = Comment.objects.get(id=comment_id, creator=user)
+            comment.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Comment.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
